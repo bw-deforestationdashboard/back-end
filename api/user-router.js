553734694c003474ca4116dd86bc6e58fe10acb6
Christian-Ford <fjord.christian@gmail.com>
2019-08-28 17:29:10 -0500

    User router updated

--- api/user-router.js
+++ api/user-router.js
@@ -13,4 +13,29 @@ router.get('/users', restricted, (req, res) => {
         });
 });
 
+router.put('/users/:id', (req, res) => {
+    Users.update(req.params.id, req.body)
+
+        .then(updated => {
+            res.status(201).json(updated);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
+});
+
+// delete contacts
+router.delete('/users/:id', (req, res) => {
+    let deleteUser = req.params.id;
+
+    Contacts.remove(deleteUser)
+        .then(deleted => {
+            res.status(201).json(deleted);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
+});
+
+
 module.exports = router;
